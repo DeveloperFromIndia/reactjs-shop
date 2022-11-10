21eeb08bf473a9c7abaf3954c29a1c54e31b6384
Goncharov Oleksii <gnchrv.dll@gmail.com>
2022-11-10 20:46:24 +0200

    Init .env & db
    Init .env file, was add db.js file in which the db connection config. In
    index.js i add connection to db & reconciliation with the data schema

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,22 @@
-import express from 'express';
+import {} from "dotenv/config";
+import express from "express";
+
+import sequelize  from "./db.js";
+
 
 const PORT = process.env.PORT || 3001;
 
+
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`<Server started on port ${PORT}/>`);
-})
+const start = async () => {
+    try {
+        await sequelize.authenticate(); // Connection to db 
+        await sequelize.sync(); // Reconciliation with the data schema
+        app.listen(PORT, () => { console.log(`<Server started on port ${PORT}/>`); })
+    } catch (e) {
+        console.log(e)
+    }
+}
 
-app.get("/api", (req, res) => {
-    res.json({
-        message:"Now there's nothing here"
-    })
-})
+start();
