259ee53fa957767202b1774c017dbf5471caf796
Goncharov Oleksii <gnchrv.dll@gmail.com>
2022-11-10 20:15:24 +0200

    Updating backend dependencies
    Was install: cors, dotenv, pg, pg-hstore, sequelize

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import Toolbar from "./Toolbar/Toolbar";
+
+const Header = () => {
+    return (
+        <header>
+            <Toolbar/>
+
+        </header>
+    );
+}
+
+
+export default Header;
---
+++ frontend/src/components/Header/Toolbar/Toolbar.jsx
@@ -0,0 +1,11 @@
+
+const Toolbar = () => {
+    return (
+        <div className="fluid-container">
+            <div className="row">row</div>
+        </div>
+    );
+}
+
+
+export default Toolbar;
