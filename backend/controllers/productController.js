fb2b3c2cf5b2f98b8d040598183c39157a1feeb3
Goncharov Oleksii <gnchrv.dll@gmail.com>
2022-11-19 17:47:38 +0200

    Some work with controllers
    Add uuid for unique filename

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,22 @@
+import UUID  from "uuid";
+import path from "path";
+import { Product } from "../models/models.js";
+import { Product_img } from "../models/models.js";
+
 class ProductController {
     async create(req, res) {
-        
+        const { vendor_code, 
+            price, price_old, 
+            amount, min_order_amount, max_order_amount, 
+            hiden } = req.body;
+
+        const { img } = req.files;
+        let file_name = UUID.v4() + ".jpg";
+        img.mv(path.resolve(__dirname, '..', 'static', file_name));
+        if (false) {
+            const product = await Product.create({vendor_code, price, price_old,amount, min_order_amount, max_order_amount});
+            const product_img = await Product_img.create({});
+        }
     } 
     async getAll(req, res) {
         
