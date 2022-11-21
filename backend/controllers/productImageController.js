31dc9563efd5f7774743308b36ae955f77d65738
Goncharov Oleksii <gnchrv.dll@gmail.com>
2022-11-21 14:16:01 +0200

    Fix productImageController

--- backend/controllers/productImageController.js
+++ backend/controllers/productImageController.js
@@ -1,8 +1,29 @@
 import ApiError from "../Error/ApiError.js";
-import { Product_img } from "../models/models.js";
+import { Product, Product_img } from "../models/models.js";
 import { v4 as uuidv4 } from "uuid";
 import cloudinary from "../utils/cloudinary.js";
 import { unlink } from 'fs';
+
+
+
+function deleteTmpImg(img) {
+    if (img !== undefined || img !== null) {
+        if (Array.isArray(img)) {
+            img.forEach(e => {
+                unlink(e.tempFilePath, (err) => {
+                    if (err !== null) { console.error(err); }
+                });
+            });
+        } else {
+            unlink(img.tempFilePath, (err) => {
+                if (err !== null) { console.error(err); }
+            });
+            return true;
+        } 
+    } else {
+        return false;
+    }
+}
 
 class ProductImageController {
     async add(req, res, next) {
@@ -13,6 +34,11 @@ class ProductImageController {
                 const { img } = req.files;
                 let result = [];
                 const product_img = await Product_img.findAll({where:{ productId }});
+                const product = await Product.findByPk(productId);
+                if(product === null) {
+                    deleteTmpImg(img);
+                    return next(ApiError.badRequest("PRODUCT NOT EXISTS"))
+                }
                 
                 let last_index = null;
                 if(product_img.length > 0) {
@@ -20,6 +46,7 @@ class ProductImageController {
                 } else {
                     last_index = 1; 
                 }
+
                 if (Array.isArray(img)) {
                     for(let i = 0; i < img.length; i++, last_index++) {
                         const file_name = uuidv4();
@@ -30,15 +57,9 @@ class ProductImageController {
                         });
                         const { url } = file_upload; 
                         
-                        const product_img = await Product_img.create({url, file_name, index: last_index, productId});
+                        const product_img = await Product_img.create({url, file_name, last_index, productId});
                         result.push(product_img);
                     }
-
-                    img.forEach(e => {
-                        unlink(e.tempFilePath, (err) => {
-                            console.error(err);
-                        });
-                    });
                 } else {
                     const file_name = uuidv4();
                     const file_upload = await cloudinary.uploader.upload(img.tempFilePath, {
@@ -46,17 +67,13 @@ class ProductImageController {
                         resource_type: "auto",
                         folder: process.env.CLOUD_PRODUCT_FOLDER
                     });
-
                     const { url } = file_upload; 
                     
                     const product_img = await Product_img.create({url, file_name, index: last_index, productId});
                     result.push(product_img);
-                    
-                    unlink(img.tempFilePath, (err) => {
-                        console.error(err);
-                    });
                 }
-                
+
+                deleteTmpImg(img);
                 return res.json(result);
             } else {
                 next(ApiError.badRequest("FILE UNDEFINED OR WRONG ID"));
