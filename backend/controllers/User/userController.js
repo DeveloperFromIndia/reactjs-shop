d7ec54925c946dd1338a156e8b84c2d6596ee809
Goncharov Oleksii <gnchrv.dll@gmail.com>
2022-11-21 18:14:31 +0200

    update structure

--- backend/controllers/User/userController.js
+++ backend/controllers/User/userController.js
@@ -7,7 +7,7 @@ class UserController {
     async login(req, res) {
 
     }
-    async check(req, res, next) {
+    async check(req, res, next) { 
         const { id } = req.query;
         if (!id) {
             return next(ApiError.badRequest('VALUE UNDEFINED OR NULL'));
